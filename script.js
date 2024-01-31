cdede8f53da44d93c7303711358464bcb7d1854b
Mark Babirad <mbabirad@brainstation.io>
2024-01-31 13:30:03 -0800

    initialize project

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Howdy! Welcome to the Control Flow script");
+
+let welcomeDemoMessage = "Welcome to the Control Flow script";
+
+console.log(welcomeDemoMessage);
+// alert(welcomeDemoMessage);
+
+console.log(1);
+console.log(2);
+console.log(6);
+
+console.log(3);
+console.log(4);
+console.log(5);
